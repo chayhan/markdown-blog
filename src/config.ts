4f67a13b85289f3d0784b2fffac54579e434a2d8
m0ozeh <kcw2205@naver.com>
2023-10-12 10:56:05 +0900

    Fix `config.ts`

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const prefix = process.env.NODE_ENV === "production" ? `https://raw.githubusercontent.com/johannblue/johannblue.github.io/main/public` : "";
+export const prefix = process.env.NODE_ENV === "production" ? `https://raw.githubusercontent.com/johannblue/markdown-blog/main/public` : "";
